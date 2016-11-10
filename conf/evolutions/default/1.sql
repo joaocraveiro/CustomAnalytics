d9ed1f0b5f8c8229750059859e93e9e4d6e8abc8
João Amaral Craveiro <joao.craveiro@intersectyon.com>
2016-11-10 11:50:03 +0000

    local postgres

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,25 +4,23 @@
 # --- !Ups
 
 create table aura (
-  id                            bigint not null,
+  id                            bigserial not null,
   name                          varchar(255),
   admin_token                   varchar(255),
   user_token                    varchar(255),
   constraint pk_aura primary key (id)
 );
-create sequence aura_seq;
 
 create table metric (
-  id                            bigint not null,
+  id                            bigserial not null,
   name                          varchar(255),
   redirect_address              varchar(255),
   aura_id                       bigint,
   constraint pk_metric primary key (id)
 );
-create sequence metric_seq;
 
 create table metric_display (
-  id                            bigint not null,
+  id                            bigserial not null,
   name                          varchar(255),
   aura_id                       bigint,
   metric_id                     bigint,
@@ -32,10 +30,9 @@ create table metric_display (
   time_frame                    varchar(255),
   constraint pk_metric_display primary key (id)
 );
-create sequence metric_display_seq;
 
 create table metric_entry (
-  id                            bigint not null,
+  id                            bigserial not null,
   category                      varchar(255),
   date                          timestamp,
   value                         integer,
@@ -43,15 +40,13 @@ create table metric_entry (
   profile_id                    bigint,
   constraint pk_metric_entry primary key (id)
 );
-create sequence metric_entry_seq;
 
 create table profile (
-  id                            bigint not null,
+  id                            bigserial not null,
   name                          varchar(255),
   register_date                 timestamp,
   constraint pk_profile primary key (id)
 );
-create sequence profile_seq;
 
 alter table metric add constraint fk_metric_aura_id foreign key (aura_id) references aura (id) on delete restrict on update restrict;
 create index ix_metric_aura_id on metric (aura_id);
@@ -71,33 +66,28 @@ create index ix_metric_entry_profile_id on metric_entry (profile_id);
 
 # --- !Downs
 
-alter table metric drop constraint if exists fk_metric_aura_id;
+alter table if exists metric drop constraint if exists fk_metric_aura_id;
 drop index if exists ix_metric_aura_id;
 
-alter table metric_display drop constraint if exists fk_metric_display_aura_id;
+alter table if exists metric_display drop constraint if exists fk_metric_display_aura_id;
 drop index if exists ix_metric_display_aura_id;
 
-alter table metric_display drop constraint if exists fk_metric_display_metric_id;
+alter table if exists metric_display drop constraint if exists fk_metric_display_metric_id;
 drop index if exists ix_metric_display_metric_id;
 
-alter table metric_entry drop constraint if exists fk_metric_entry_metric_id;
+alter table if exists metric_entry drop constraint if exists fk_metric_entry_metric_id;
 drop index if exists ix_metric_entry_metric_id;
 
-alter table metric_entry drop constraint if exists fk_metric_entry_profile_id;
+alter table if exists metric_entry drop constraint if exists fk_metric_entry_profile_id;
 drop index if exists ix_metric_entry_profile_id;
 
-drop table if exists aura;
-drop sequence if exists aura_seq;
+drop table if exists aura cascade;
 
-drop table if exists metric;
-drop sequence if exists metric_seq;
+drop table if exists metric cascade;
 
-drop table if exists metric_display;
-drop sequence if exists metric_display_seq;
+drop table if exists metric_display cascade;
 
-drop table if exists metric_entry;
-drop sequence if exists metric_entry_seq;
+drop table if exists metric_entry cascade;
 
-drop table if exists profile;
-drop sequence if exists profile_seq;
+drop table if exists profile cascade;
 
